0fc2d8f1b457d46692d9bbb2b77e4500e0c0ca0a
guru2396 <gururajm2396@gmail.com>
2022-04-04 22:32:26 +0530

    First commit

---
+++ src/main/java/com/centraldbserver/centraldbserver/CentraldbserverApplication.java
@@ -0,0 +1,13 @@
+package com.centraldbserver.centraldbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentraldbserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentraldbserverApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/centraldbserver/centraldbserver/config/JwtFilter.java
@@ -0,0 +1,46 @@
+package com.centraldbserver.centraldbserver.config;
+
+import com.centraldbserver.centraldbserver.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    //@Autowired
+    //private PatientAppService patientAppService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String auth=request.getHeader("Authorization");
+        if(auth!=null && !"".equals(auth) && auth.startsWith("Bearer ")){
+            String subject=jwtService.extractID(auth);
+            if(subject!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+                if(subject.startsWith("PAT_")){
+                    //Patient_info patient=patientAppService.getPatientById(subject);
+                    if(true){
+                        UsernamePasswordAuthenticationToken ut=new UsernamePasswordAuthenticationToken(null,null,null);
+                        ut.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(ut);
+                    }
+
+                }
+            }
+
+        }
+        filterChain.doFilter(request,response);
+    }
+}
